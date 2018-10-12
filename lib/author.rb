a2f5c30909e20faaea547048bc5fb1fd7efb83f8
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-10-12 15:47:18 +0100

    Done

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+  attr_accessor :name, :author
+
+  @@posts = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    @@posts
+  end
+
+  def add_post(post)
+    post.author = self
+    @@posts << post
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.title = title
+    post.author = self
+    @@posts << post
+  end
+
+  def self.post_count
+    @@posts.count
+  end
+  
+end
